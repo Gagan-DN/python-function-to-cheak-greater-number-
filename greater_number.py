4e6a9e9660aaae99de2d86717b684f442b62c04b
Gagan-DN <116274483+Gagan-DN@users.noreply.github.com>
2024-01-06 22:12:54 +0530

    Create greater_number.py

---
+++ greater_number.py
@@ -0,0 +1,9 @@
+#2 check a<b
+def checkg(a,b,c):
+    if a>b:
+        if not a==c:
+            print("a>b and a is not = to c")
+    else:
+         print("condition is not stisfied")
+        
+checkg(4,2,5)
